e72e67f1b6e6db929ea8d0adec8a41397bce1086
IT-Kuantum <IT-Kuantum@yandex.ru>
2020-10-06 17:30:15 +1000

    1

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 print('Это мои первые строчки кода')
 print(1+2)
+print(2 + 4 // 6 * 10)
+print(9 // 4)
+name = 10
+print(name)
+i = 'Это мои первые строчки кода'
+n = '10'
+m = 20
+print(n + str(m))
+print(f'{n}{m}')
+j = f'{m} "-это переменная"'
+print(j)
+print(m, n)
+name1 = int(input('Введите число '))
+name2 = int(input('Введите число '))
+print(name1 + name2)
