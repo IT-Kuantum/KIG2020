3a8ce157ed46fdbd752f658e060bea7561ea8a24
IT-Kuantum <IT-Kuantum@yandex.ru>
2020-10-08 16:47:51 +1000

    ;

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-print('Это мои первые строчки кода')
-print(1+2)
-print(2 + 4 // 6 * 10)
-print(9 // 4)
-name = 10
-print(name)
-i = 'Это мои первые строчки кода'
-n = '10'
-m = 20
-print(n + str(m))
-print(f'{n}{m}')
-j = f'{m} "-это переменная"'
-print(j)
-print(m, n)
-name1 = int(input('Введите число '))
-name2 = int(input('Введите число '))
-print(name1 + name2)
+name1 = 0
+name2 = 0
+
+if name1 == name2:
+    print('name1 = name2')
+    if name2 is True:
+        print('True')
+elif name1 < name2:
+    print ('name1 < name2')
+elif name1 != name2:
+    pass
+else:
+    print('name1 = name2')
