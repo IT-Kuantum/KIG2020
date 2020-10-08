f1f641eda042249d6c929b6cdfc7d5bd8ce247bf
IT-Kuantum <IT-Kuantum@yandex.ru>
2020-10-08 17:22:34 +1000

    1

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+'''
+#Работа с условными операторами
+
 name1 = 0
 name2 = 0
 
@@ -11,3 +14,31 @@ elif name1 != name2:
     pass
 else:
     print('name1 = name2')
+'''
+
+'''
+#Работа с циклам for
+for i in 1, 2, 3, 'sorry':
+    print(i)
+
+for i in 'sorry':
+    print(i)
+'''
+
+'''
+#Список [], Кортедж ()
+n = [1, 2, 3, 'sorry']
+print(n[0])
+print(len(n))
+n.append(7)
+print(n)
+'''
+
+'''
+#Работа с циклом while
+m = 0
+#while True - бесконечный цикл
+while m < 10:
+    print(m)
+    m += 1
+'''
