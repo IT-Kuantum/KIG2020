80e20f901394c5d807084057993eadcdf6b0bfd9
IT-Kuantum <IT-Kuantum@yandex.ru>
2020-10-20 17:30:47 +1000

    0

--- main.py
+++ main.py
@@ -43,36 +43,53 @@ while m < 10:
     m += 1
 '''
 
-import pygame 
-import random 
+import pygame
+import random
 
 num = random.randint(0, 100)
 print(num)
-
 W = 480
 H = 360
-SILVER = (192, 192, 192)
+WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-
+numeral = ''
+move = 1
+block = 0
+start = 1
+OUISIDE_BG = (0, -100)
 
 pygame.init()
-pygame.display.set_caption('ИГРРАААА') 
-pygame.mouse.set_visible(False)
+pygame.display.set_caption('угадай число')
 screen = pygame.display.set_mode((W, H))
+pygame.mouse.set_visible(False)
 
-bg = pygame.image.load('Image/room.png')
+font = pygame.font.SysFont('Arial', 28, True, False)
+font_box = pygame.Surface((W - font.get_height(), font.get_height()))
+font_rect = font_box.get_rect(center=(W // 2, H - font.get_height()))
+font2 = pygame.font.SysFont('Arial', 14, False, True)
+
+bg = pygame.image.load('image/room.png')
 bg_rect = bg.get_rect(topleft=(0, 0))
-cat = pygame.image.load('Image/cat.png')
+cat = pygame.image.load('image/cat.png')
 cat_rect = cat.get_rect(center=(70, 220))
-dog = pygame.image.load('Image/dog.png')
+dog = pygame.image.load('image/dog.png')
 dog_rect = dog.get_rect(center=(410, 220))
-owl = pygame.image.load('Image/owl.png')
+owl = pygame.image.load('image/owl.png')
 owl_rect = owl.get_rect(center=(210, 120))
-dialog = pygame.image.load('Image/dialog.png')
-dialog_rect = dialog.get_rect() 
+dialog = pygame.image.load('image/dialog.png')
+dialog_rect = dialog.get_rect()
+dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
+dialog_dog_pos = (dog_rect.x - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
+dialog_owl_pos = (owl_rect.x, owl_rect.y - dialog_rect.h)
+
 
 def dialogs(text, pos, owl_text):
-    screen.blit
+    screen.blit(dialog, pos)
+    screen.blit(font2.render(text, True, BLACK), (pos[0] + 5, pos[1] + 5))
+    screen.blit(dialog, dialog_owl_pos)
+    screen.blit(font2.render(owl_text, True, BLACK), (dialog_owl_pos[0] + 5, dialog_owl_pos[1] + 5))
+    pygame.display.update()
+    pygame.time.wait(2000)
 
 # Цикл while в игре
 run = True
@@ -115,15 +132,15 @@ while run:
                     move = 1
 
     if block = 0:
-     screen.blit(bg, bg_rect)
-     screen.blit(cat, cat_rect)
-     screen.blit(dog, dog_rect)
-     screen.blit(owl, owl_rect)
-     screen.blit(font_box, font_rect)
-     font_box.fill(SILVER)
-    font_box.blit(
+        screen.blit(bg, bg_rect)
+        screen.blit(cat, cat_rect)
+         screen.blit(dog, dog_rect)
+        screen.blit(owl, owl_rect)
+         screen.blit(font_box, font_rect)
+         font_box.fill(SILVER)
+        font_box.blit(
         font.render(numeral, True, BLACK), (10, 0)
-    )
+     )
     pygame.display.update() 
 
     if start == 1:
