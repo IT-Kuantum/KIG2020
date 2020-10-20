c5ec174b30155965b7ce6fc85095fb805e549090
IT-Kuantum <IT-Kuantum@yandex.ru>
2020-10-20 16:57:20 +1000

    0

--- main.py
+++ main.py
@@ -43,3 +43,43 @@ while m < 10:
     m += 1
 '''
 
+
+# Цикл while в игре
+run = True
+while run:
+    for e in pygame.event.get():
+        if e.type == pygame.quit:
+            run = False
+        # Выход из игры с помощью escape
+        elif e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_ESCAPE:
+                run = False
+            # Печатает только цифры
+            elif e.unicode.isdecimal() and block == 0:
+                numeral += e.unicode
+            # Удаляет последний элемент
+            elif e.key == pygame.K_BACKSPACE:
+                numeral = numeral [:-1]
+            elif e.key == pygame.K_RETURN and numeral:
+                if int(numeral) > 100:
+                    dialogs('', OUTSIDE_BG, 'Вы ошиблись')
+                elif int(numeral) > num:
+                    dialogs('', OUTSIDE_BG, 'Число меньше')
+                elif int(numeral) < num:
+                    dialogs('', OUTSIDE_BG, 'Число больше')
+                if move == 1:
+                    if int(numeral) == num:
+                        dialogs (f'Это число {numeral}', dialog_cat_pos, 'Кот ты выиграл')
+                        block = 1
+                    else:
+                        dialogs('Дог, твой ход', dialog_dog_pos, 'Продолжаем')
+                elif move == 2:
+                    if int(numeral) == num:
+                        dialogs (f'Это число {numeral}', dialog_cat_pos, 'Дог ты выиграл')
+                        block = 1
+                    else:
+                        dialogs('Кот, твой ход', dialog_dog_pos, 'Продолжаем')
+                numeral = ''
+                move += 1
+                if move > 2:
+                    move = 1
